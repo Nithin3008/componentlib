9584cb7d1f5e74672ebe4aa5b291a1034ae5eeaf
Nithin <nithindevathi@gmail.com>
2023-09-05 15:38:31 +0530

    Added avatars styling

--- src/components/Avatar/Avatars.jsx
+++ src/components/Avatar/Avatars.jsx
@@ -1,5 +1,23 @@
+import "./Avatar.css";
 const Avatars = () => {
-  return <div>Avatars</div>;
+  return (
+    <div>
+      <p>
+        An avatar is a computer representation of users in a computer-generated
+        3D world, used primarily in chat and entertainment web sites.
+      </p>
+      <hr></hr>
+      <div className="avatars">
+        <img src="/avatar.jpg" className="small"></img>
+        <img src="/avatar.jpg" className="medium"></img>
+        <img src="/avatar.jpg" className="medium-square"></img>
+        <img src="/avatar.jpg" className="large"></img>
+      </div>
+      <div className="code">
+        <img src="avatars.png"></img>
+      </div>
+    </div>
+  );
 };
 
 export default Avatars;
